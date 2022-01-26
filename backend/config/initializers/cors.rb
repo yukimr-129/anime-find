fe487c49cc0c60b61f9334696e61f21e120be4b8
山村勇貴 <yamamurayuuki@yamamurayuuki-no-MacBook-Pro.local>
2022-01-26 17:59:54 +0900

    cors.rb 環境変数設定

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins ENV.fetch("FRONT_BASE_URL") {"http://localhost:3000"}
 
     resource '*',
       headers: :any,
