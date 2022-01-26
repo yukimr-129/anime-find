6ebbe789cdc2d3e8290e22b192ce8b68e1f5ddd6
山村勇貴 <yamamurayuuki@yamamurayuuki-no-MacBook-Pro.local>
2022-01-26 16:09:47 +0900

    フロン側に本番環境のURL設定

--- frontend/react-app/src/lib/api/client.ts
+++ frontend/react-app/src/lib/api/client.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 
+const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_RAILS_API_URL
 export const client = axios.create({
-  baseURL: process.env.REACT_APP_RAILS_API_URL
+  baseURL: apiUrl
 })
