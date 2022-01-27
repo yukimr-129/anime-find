7f87268c99379131df272dad2733aa2e70a2eef5
山村勇貴 <yamamurayuuki@yamamurayuuki-no-MacBook-Pro.local>
2022-01-27 22:57:48 +0900

    APIURL設定修正/ cors設定修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONT_BASE_URL") {"http://localhost:3000"}
+    origins 'http://localhost:3000', 'https://anime-find-api.herokuapp.com/api/v1'
 
     resource '*',
       headers: :any,
