a9f123cfb1cbcf34b0b56f17f417c4e30238ee09
山村勇貴 <yamamurayuuki@yamamurayuuki-no-MacBook-Pro.local>
2022-01-27 22:34:13 +0900

    APIのURL通るか確認

--- frontend/react-app/src/lib/api/client.ts
+++ frontend/react-app/src/lib/api/client.ts
@@ -2,5 +2,5 @@ import axios from "axios"
 
 const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_RAILS_API_URL
 export const client = axios.create({
-  baseURL: apiUrl
+  baseURL: 'https://anime-find-api.herokuapp.com/api/v1'
 })
